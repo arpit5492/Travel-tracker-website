62c4de0cb0a9fa504e4edf615d0422a71989c097
arpit5492 <arpitmallick99@gmail.com>
2023-12-28 16:06:15 -0800

    1st part done

--- index.js
+++ index.js
@@ -6,12 +6,31 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
+// Connection to Database
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "firstDB",
+  password: "password",
+  port: 5433
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(morgan("dev"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  console.log(result.rows);
+  let countries = [];
+  result.rows.forEach(country => {
+    countries.push(country.country_code);
+  });
+  console.log(countries);
+  res.render("index.ejs", {countries: countries, total: countries.length});
 });
 
 app.listen(port, () => {
